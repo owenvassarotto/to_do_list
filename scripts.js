217fdded40010578a49191c1b0a7f895757e67f1
ovass <vassarottowen@gmail.com>
2023-04-06 17:36:03 -0300

    Added an icon for the page

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+const btnAdd = document.getElementById('btn-add');
+
+function addTask(){
+    console.log("Adding a task")
+}
+
+btnAdd.addEventListener("click", addTask);
